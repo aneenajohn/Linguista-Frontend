322a86f7bbd3d217cce722c8e8440d73e92334b5
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-07-08 06:03:36 +0530

    initial commit

---
+++ linguista-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hellooooooo
+      </header>
+    </div>
+  );
+}
+
+export default App;
